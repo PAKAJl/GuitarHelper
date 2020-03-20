87d14bc1a52f653c757fab9c7e52b327fe762608
PAKAJl <dgolubev7890@gmail.com>
2020-03-20 11:47:18 +0300

    Initial commit

---
+++ CourseWork/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace CourseWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool[] checkOnClick;
+        private static Button[] buttonsList;
+        public double freq;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ScanSoundCards();
+            checkOnClick = new bool[2];
+            buttonsList = new Button[2];
+            buttonsList[0] = button0;
+            buttonsList[1] = button1;
+            for (int i = 0; i < checkOnClick.Length; i++)
+            {
+                checkOnClick[i] = false;
+            }
+        }
+        
+        private void ScanSoundCards()
+        {
+            cdDevice.Items.Clear();
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+                cdDevice.Items.Add(NAudio.Wave.WaveIn.GetCapabilities(i).ProductName);
+            if (cdDevice.Items.Count > 0)
+                cdDevice.SelectedIndex = 0;
+            else
+                MessageBox.Show("ERROR: no recording devices available");
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            Sound sound = new Sound(this);
+            this.Dispatcher.BeginInvoke(new ThreadStart(delegate
+            {
+                sound.StartDetect(cdDevice.SelectedIndex);
+            }));
+        }
+        private static void ButtonAccsess(int numberButton)
+        {
+            for (int i = 0; i < checkOnClick.Length; i++)
+            {
+                checkOnClick[i] = false;
+            }
+            checkOnClick[numberButton] = true;
+            for (int i = 0; i < buttonsList.Length; i++)
+            {
+                if (!checkOnClick[i])
+                {
+                    buttonsList[i].Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF323232"));
+                }
+                else
+                {
+                    buttonsList[i].Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#525252"));
+                }
+            }
+        }  
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAccsess(0);
+            OxyPlot.Wpf.PlotView plotView = new OxyPlot.Wpf.PlotView();
+            plotView.Margin = new Thickness(0, 0, 82, 10);
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAccsess(1);
+        }
+
+    }
+}
---
+++ CourseWork/Resource/Lib/Classes/Sound.cs
@@ -0,0 +1,106 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Threading;
+
+namespace CourseWork
+{
+    public class Sound
+    {
+        private MainWindow _window;
+
+        public Sound(MainWindow window)
+        {
+            _window = window;
+        }
+
+        BufferedWaveProvider bufferedWaveProvider = null;
+        Dictionary<string, float> noteBaseFreqs = new Dictionary<string, float>()
+            {
+                { "C", 16.35f },
+                { "C#", 17.32f },
+                { "D", 18.35f },
+                { "Eb", 19.45f },
+                { "E", 20.60f },
+                { "F", 21.83f },
+                { "F#", 23.12f },
+                { "G", 24.50f },
+                { "G#", 25.96f },
+                { "A", 27.50f },
+                { "Bb", 29.14f },
+                { "B", 30.87f },
+            };
+
+
+        public void StartDetect(int inputDevice)
+        {
+           
+                WaveInEvent waveIn = new WaveInEvent();
+
+                waveIn.DeviceNumber = inputDevice;
+                waveIn.WaveFormat = new WaveFormat(44100, 1);
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+
+                bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
+
+                // begin record
+                waveIn.StartRecording();
+
+                IWaveProvider stream = new Wave16ToFloatProvider(bufferedWaveProvider);
+                Pitch pitch = new Pitch(stream);
+
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+
+                do
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    float freq = pitch.Get(buffer);
+
+                    if (freq != 0)
+                    {
+                    //вывод частоты
+                    
+                    }
+
+                } while (bytesRead != 0);
+
+                // stop recording
+                waveIn.StopRecording();
+                waveIn.Dispose();
+        }
+
+        void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (bufferedWaveProvider != null)
+            {
+                bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+                bufferedWaveProvider.DiscardOnBufferOverflow = true;
+            }
+        }
+
+        public string GetNote(float freq)
+        {
+            float baseFreq;
+
+            foreach (var note in noteBaseFreqs)
+            {
+                baseFreq = note.Value;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if ((freq >= baseFreq - 1) && (freq < baseFreq + 1) || (freq == baseFreq))
+                    {
+                        return note.Key + i;
+                    }
+
+                    baseFreq *= 2;
+                }
+            }
+
+            return null;
+        }
+    }
+}
